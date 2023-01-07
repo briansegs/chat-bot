841d78526304395fdc372fac0bf8c87eee5a23d3
brian <briansegs@gmail.com>
2023-01-06 19:40:16 -0500

    feat: Add handel Submit function

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,37 @@
 import logo from './logo.svg';
 import './normal.css';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [input, setInput] = useState("");
+  const [chatLog, setChatLog] = useState([{
+    user: "gpt",
+    message: "How can I help you today?"
+  },{
+    user: "me",
+    message: "I want to use chatGPT today!"
+  }]);
+
+  async function handeleSubmit(e) {
+    e.preventDefault();
+    setChatLog([...chatLog, { user: "me", message: `${input}`}])
+    setInput("");
+
+    const response = await fetch("http://localhost:3080/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        message: chatLog.map((message) => message.message).join("")
+      })
+    });
+    const data = await response.json();
+    console.log(data);
+  }
+
   return (
     <div className="App">
       <aside className="sidemenu">
@@ -15,37 +44,42 @@ function App() {
       </aside>
       <section className="chatbox">
       <div className='chat-log'>
-          <div className='chat-message'>
-            <div className='chat-message-center'>
-            <div className='avatar'>
-
-              </div>
-              <div className='message'>
-                Hello World
-              </div>
-            </div>
-          </div>
-          <div className='chat-message chatgpt'>
-            <div className='chat-message-center'>
-              <div className='avatar chatgpt'>
-
-              </div>
-              <div className='message'>
-                I am an AI
-              </div>
-            </div>
-          </div>
+          {chatLog.map((message, index) => (
+            <ChatMessage
+              key={index}
+              message={message}
+            />
+          ))}
       </div>
         <div className="chat-input-holder">
-          <textarea
-          className="chat-input-textarea"
-          rows="1"
-          >
-          </textarea>
+          <form onSubmit={handeleSubmit}>
+            <input
+            className="chat-input-textarea"
+            rows="1"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            >
+            </input>
+          </form>
         </div>
       </section>
     </div>
   );
+}
+
+const ChatMessage = ({message}) => {
+  return (
+    <div className={`chat-message ${message.user === "gpt" && "chatgpt"}`}>
+      <div className='chat-message-center'>
+        <div className={`avatar ${message.user === "gpt" && "chatgpt"}`}>
+          {message.user === "gpt"}
+        </div>
+        <div className='message'>
+          {message.message}
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default App;
