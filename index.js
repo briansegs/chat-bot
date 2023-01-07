5e95f729662a82c714548791a81df1f12fa7ab41
brian <briansegs@gmail.com>
2023-01-06 21:24:47 -0500

    feat: post request

--- index.js
+++ index.js
@@ -1,25 +1,30 @@
 const { Configuration, OpenAIApi } = require("openai");
 const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const apiKey = require('./config')
 
 const configuration = new Configuration({
     organization: "org-n5YJ8lN7b4DFAEcdZrOXzRI1",
-    apiKey: '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',
+    apiKey: apiKey.apiKey,
 });
 const openai = new OpenAIApi(configuration);
-
 const app = express()
-const port = 3000
+app.use(bodyParser.json())
+app.use(cors())
+const port = 3080
 
 app.post('/',async (req, res) => {
+    const { message } = req.body;
+    console.log("message: ", message.data)
     const response = await openai.createCompletion({
         model: "text-davinci-003",
-        prompt: "Say this is a test",
-        max_tokens: 7,
-        temperature: 0,
+        prompt: `${message}`,
+        max_tokens: 100,
+        temperature: 0.5,
         });
-    console.log(response.data.choices[0].text)
     res.json({
-        data: response.data
+        message: response.data.choices[0].text,
     })
 });
 
